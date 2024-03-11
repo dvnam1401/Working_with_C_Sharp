051322db415be03f2e0bd794d86d81a577ccac31
Văn Nam <vannamdang2003@gmail.com>
2024-03-12 01:06:40 +0700

    up

--- Learn to build an e-commerce store with .Net, React & Redux/ReStore/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ Learn to build an e-commerce store with .Net, React & Redux/ReStore/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa93c5904f82833fa0c9db795c3b95583f4f9606")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541e6b9a4fe4c861159afa2c6904fcea3e8189b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

20dec0ef50f2bf656c0840953b170233a8291817
Văn Nam <vannamdang2003@gmail.com>
2024-03-13 11:52:27 +0700

    end of section 3

--- Net_ReactAndRedux/ReStore/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ Net_ReactAndRedux/ReStore/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051322db415be03f2e0bd794d86d81a577ccac31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e7381e1bc72d20733228cb832c85d0168922ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Net_ReactAndRedux/ReStore/client/src/features/catalog/ProductList.tsx
+++ Net_ReactAndRedux/ReStore/client/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-import { List } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
 
@@ -8,10 +8,12 @@ interface Props {
 
 export default function ProductList(props: Props) {
     return (
-        <List>
+        <Grid container spacing={2}>
             {props.products.map(product => (
-                <ProductCard key={product.id} product={product}></ProductCard>
+                <Grid item xs={3} key={product.id}>
+                    <ProductCard product={product}></ProductCard>
+                </Grid>
             ))}
-        </List>
+        </Grid>
     )
 }
